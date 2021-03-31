b0b3763ef2d1e5bc9ddb86ac6a14e7ba5fb01f48
huni <huni@emmental.co.kr>
2021-03-31 20:14:05 +0900

    ✨twin.macro 설치 및 설정

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GlobalStyles } from 'twin.macro';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
