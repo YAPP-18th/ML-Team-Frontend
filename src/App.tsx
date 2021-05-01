09faa444babf486a6bf741a810bca41dd5ebd77d
huni <huni@emmental.co.kr>
2021-05-01 17:05:07 +0900

    fix: App.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-/** @jsxRuntime classic */
-/** @jsx jsx */
-import { jsx } from '@emotion/react';
 import React from 'react';
 import 'twin.macro';
-import logo from './logo.svg';
 import './App.css';
 import Router from './Router';
 
