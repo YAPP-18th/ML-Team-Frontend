23661ee2b7e08086d881c0c7137fe63f1b72afcb
huni <huni@emmental.co.kr>
2021-05-01 17:01:05 +0900

    feat: 디자인 시스템 colors Tailwind 테마 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,40 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      gray: {
+        1: '#FFFFFF',
+        2: '#FAFAFA',
+        3: '#F5F5F5',
+        4: '#F0F0F0',
+        5: '#D9D9D9',
+        6: '#BFBFBF',
+        7: '#8C8C8C',
+        8: '#595959',
+        9: '#434343',
+        10: '#262626',
+        11: '#1F1F1F',
+        12: '#141414',
+        13: '#000000',
+        default: '#1F1F1F',
+      },
+      primary: {
+        2: '#E9E9FF',
+        3: '#D2D2FF',
+        4: '#C5C4FF',
+        5: '#BCBAFF',
+        6: '#A7A5FF',
+        7: '#9290FB',
+        8: '#8280F5',
+        9: '#5C5ADD',
+        10: '#4745CB',
+        11: '#3C39C0',
+        default: '#4745CB',
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
