aa3361ebb5713a5dd8fa5ce8c8bf4bf2fd4dd5fc
huni <huni@emmental.co.kr>
2021-05-09 22:44:33 +0900

    feat: 메인 레이아웃 스타일 작업

--- src/components/layouts/main/MainLayout.tsx
+++ src/components/layouts/main/MainLayout.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Layout, Menu } from 'antd';
 import { Content, Footer, Header } from 'antd/es/layout/layout';
 import Logo from '@assets/images/logo.svg';
 import 'twin.macro';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-import { GRAY_10 } from '@shared/styles/colors';
+import { GRAY_10, PRIMARY_8 } from '@shared/styles/colors';
+import tw from 'twin.macro';
 
-const StyledRestrictedWidthWrapper = styled.div`
+const StyledHeaderInner = styled.div`
   max-width: 1194px;
   width: 100%;
   margin: 0 auto;
   padding: 0 30px;
   box-sizing: content-box;
+`;
+
+const FooterStyle = css`
+  width: 100%;
+  padding: 20px 30px;
+`;
+
+const StyledFooterInner = styled.div`
+  max-width: 1194px;
+  margin: 0 auto;
+
+  ${tw`flex justify-between items-center text-gray-3 font-medium border-t border-gray-9 py-5`}
+`;
+
+const MenuStyle = css`
+  margin-left: 45px;
+  background: transparent !important;
 `;
 
 export const MainLayout: React.FC = ({ children }) => {
@@ -26,27 +44,20 @@ export const MainLayout: React.FC = ({ children }) => {
             box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.25);
           `}
         >
-          <StyledRestrictedWidthWrapper tw="flex">
+          <StyledHeaderInner tw="flex">
             <img src={Logo} tw="self-center" alt="STUDEEP" />
-            <Menu
-              theme="dark"
-              mode="horizontal"
-              defaultSelectedKeys={['2']}
-              css={css`
-                margin-left: 45px;
-              `}
-            >
-              <Menu.Item key="1">nav 1</Menu.Item>
-              <Menu.Item key="2">nav 2</Menu.Item>
-              <Menu.Item key="3">nav 3</Menu.Item>
+            <Menu theme="dark" mode="horizontal" css={MenuStyle}>
+              <MenuItem key="1">nav 1</MenuItem>
+              <MenuItem key="2">nav 2</MenuItem>
+              <MenuItem key="3">nav dasdnjksanjkdasjdasjk3</MenuItem>
             </Menu>
-          </StyledRestrictedWidthWrapper>
+          </StyledHeaderInner>
         </Header>
         <Content style={{ padding: '0 50px' }}>
           <div className="site-layout-content">{children}</div>
         </Content>
-        <Footer tw="bg-gray-9">
-          <StyledRestrictedWidthWrapper tw="flex justify-between items-center text-gray-3 font-medium">
+        <Footer tw="p-0" css={FooterStyle}>
+          <StyledFooterInner>
             <div tw="flex justify-between items-center space-x-10">
               <img
                 src={Logo}
@@ -61,9 +72,48 @@ export const MainLayout: React.FC = ({ children }) => {
               </ul>
             </div>
             <span tw="text-gray-6">© STUDEEP All Rights Reserved.</span>
-          </StyledRestrictedWidthWrapper>
+          </StyledFooterInner>
         </Footer>
       </Layout>
+    </>
+  );
+};
+
+const MenuItem: React.FC = ({ children, ...props }) => {
+  const MenuItemStyle = css`
+    padding: 0 5px !important;
+    margin-right: 30px;
+
+    &.ant-menu-item-selected {
+      background: transparent !important;
+
+      &::before {
+        background: ${PRIMARY_8};
+      }
+    }
+
+    &::before {
+      transition: all 0.1s;
+      background: transparent;
+      height: 4px;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      content: '';
+      right: 0;
+      width: 100%;
+    }
+
+    &:hover {
+      background: transparent !important;
+    }
+  `;
+
+  return (
+    <>
+      <Menu.Item {...props} css={MenuItemStyle}>
+        {children}
+      </Menu.Item>
     </>
   );
 };
