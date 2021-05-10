06a7fbb5f2edaafcb58996846220cf02d7701f7f
huni <huni@emmental.co.kr>
2021-05-10 22:41:46 +0900

    fix: 페이스북 로그인 disabled

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { Button } from 'antd';
+import { Button, Tooltip } from 'antd';
 import 'twin.macro';
+import { useGoogleLogin } from 'react-google-login';
 
 // typography
 import { StdTypoCaption1, StdTypoH3 } from '@shared/styled/Typography';
@@ -27,13 +28,17 @@ const Login = () => {
           </StdTypoH3>
           <div tw="mt-14 space-y-2.5">
             <Button
-              tw="w-full block bg-gray-1 hover:bg-gray-1 hover:opacity-75 text-gray-10 hover:text-gray-8 font-bold space-x-2.5"
+              tw="w-full block bg-gray-1 hover:bg-gray-1 hover:opacity-75 text-gray-10 hover:text-gray-10 font-bold space-x-2.5 focus:bg-gray-6 focus:text-gray-10"
               type="text"
             >
               <img src={GoogleImg} alt="구글 로그인" tw="inline-block" />
               구글 로그인
             </Button>
-            <Button tw="w-full block font-bold space-x-2.5" type="primary">
+            <Button
+              tw="w-full block font-bold space-x-2.5"
+              type="primary"
+              disabled
+            >
               <img src={FacebookImg} alt="페이스북 로그인" tw="inline-block" />
               페이스북으로 시작하기
             </Button>
