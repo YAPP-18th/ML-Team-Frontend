09a807a3efe1ab2d97e33a965ce616ae95d896f1
YeonjuSeo <tjduswn1219@gmail.com>
2021-05-12 04:52:36 +0900

    feat: 공부방 화면 작성

--- src/pages/study/index.tsx
+++ src/pages/study/index.tsx
@@ -1,48 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
-import { Button, Tooltip, Layout } from 'antd';
 import { jsx, css } from '@emotion/react';
-const { Header, Sider, Footer, Content } = Layout;
-// components
-import StudyRoomSide from '../../components/study/StudyRoomSide';
-// images`
-import ExitImg from '@assets/images/exit.svg';
-import SoundOffImg from '@assets/images/sound-off.svg';
-import ShareImg from '@assets/images/share.svg';
-import { GRAY_12 } from '@shared/styles/colors';
-
 import 'twin.macro';
-import { StdTypoH4 } from '@shared/styled/Typography';
+
+// components
+import { Button, Layout } from 'antd';
+import StudyRoomSide from '../../components/study/StudyRoomSide';
+import RTCVideo from '../../components/study/RTCVideo';
+const { Header, Sider, Footer, Content } = Layout;
+
+// typography
+import { StdTypoH1, StdTypoH5 } from '@shared/styled/Typography';
+
+// colors
+import {
+  GRAY_4,
+  GRAY_9,
+  GRAY_11,
+  GRAY_12,
+  PRIMARY_10,
+} from '@shared/styles/colors';
+
+// images
+import ExitImg from '@assets/images/exit.svg';
+import PrivateImg from '@assets/images/private.svg';
+import TimeImg from '@assets/images/time.svg';
 
 const Study = () => {
+  const [localStream, setLocalStream] = useState<MediaStream>();
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+      setLocalStream(stream);
+    });
+  }, []);
   return (
     <Layout>
       <Header css={HeaderStyle}>
-        <StyledStudyRoomTitle>공부방 이름</StyledStudyRoomTitle>
-        <div tw="flex items-center space-x-5">
-          <Button type="primary" shape="circle">
-            <ButtonImgStyled src={SoundOffImg} />
-          </Button>
-          <Button type="primary">
-            <ButtonContentStyled>
-              <ButtonImgStyled src={ExitImg} />
-              <div>공부 종료하기</div>
-            </ButtonContentStyled>
-          </Button>
+        <div tw="flex">
+          <StdTypoH5>공부방 이름</StdTypoH5>
+          {/* private이라면 */}
+          <img src={PrivateImg} />
         </div>
+        <Button
+          tw="bg-gray-10 border-none flex items-center hover:bg-gray-9 "
+          shape="round"
+          type="primary"
+        >
+          <ButtonImgStyled src={ExitImg} alt="공부종료" />
+          <div>공부 종료하기</div>
+        </Button>
       </Header>
       <Layout>
-        <Content>Content</Content>
-        <Sider width={465}>
-          <SideTitleWrapperStyled>
-            <div>공부중인 사람</div>
-            <Button type="primary">
-              <ButtonContentStyled>
-                <ButtonImgStyled src={ShareImg} />
-                <div>초대하기</div>
-              </ButtonContentStyled>
-            </Button>
-          </SideTitleWrapperStyled>
+        <Content tw="relative">
+          <RTCVideo width={'1544px'} mediaStream={localStream} />
+          <StyledStudyInfoBar>
+            <div tw="flex items-center">
+              <StyledStudyInfoSet>
+                <img src={TimeImg} tw="mr-1" alt="공부세트" />
+                <div>$ set</div>
+              </StyledStudyInfoSet>
+              <div tw="flex items-end">
+                <StdTypoH1>$$:$$:$$</StdTypoH1>
+                <StdTypoH5
+                  css={css`
+                    margin-left: 20px;
+                    color: ${GRAY_4};
+                  `}
+                >
+                  휴식시간까지 $$:$$:$$
+                </StdTypoH5>
+              </div>
+            </div>
+            <StyledStudyInfoStatus>
+              <StdTypoH5>상태표시</StdTypoH5>
+            </StyledStudyInfoStatus>
+          </StyledStudyInfoBar>
+        </Content>
+        <Sider style={{ backgroundColor: 'green' }} width={465}>
           <StudyRoomSide />
         </Sider>
       </Layout>
@@ -61,20 +95,42 @@ const HeaderStyle = css`
   background: ${GRAY_12};
 `;
 
-const StyledStudyRoomTitle = styled.span`
-  font-size: 22px;
-  font-weight: 600;
-`;
-
-const ButtonContentStyled = styled.div`
-  display: flex;
-`;
 const ButtonImgStyled = styled.img`
   width: 24px;
   height: 24px;
+  margin-right: 8px;
 `;
-
-const SideTitleWrapperStyled = styled.div`
+const StyledStudyInfoBar = styled.div`
+  width: 1387px;
+  height: 128px;
+  position: absolute;
+  color: white;
   display: flex;
+  top: 840px;
+  left: 34px;
+  background-color: ${GRAY_11};
+  opacity: 0.9;
+  align-items: center;
+  border-radius: 20px;
+  padding: 0px 40px;
   justify-content: space-between;
+`;
+const StyledStudyInfoStatus = styled.div`
+  background-color: ${PRIMARY_10};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  width: 175px;
+  height: 56px;
+`;
+const StyledStudyInfoSet = styled.div`
+  background-color: ${GRAY_9};
+  display: flex;
+  border-radius: 8px;
+  width: 97px;
+  height: 38px;
+  justify-content: center;
+  align-items: center;
+  margin-right: 30px;
 `;
