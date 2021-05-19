fbfc6f90b8292cee86b05d5ea824d36717e3d9c4
hoondeveloper <hoonskyn@naver.com>
2021-05-20 00:09:51 +0900

    refactor: StyledEmptyCardWrapper 컴포넌트로 분리

--- src/shared/styled/Common.tsx
+++ src/shared/styled/Common.tsx
@@ -1,8 +1,15 @@
 import styled from '@emotion/styled';
+import { GRAY_11 } from '@shared/styles/colors';
 
 export const StyledRestrictedArea = styled.section`
   max-width: 1194px;
   width: 100%;
   margin: 0 auto;
   box-sizing: content-box;
+`;
+
+export const StyledEmptyCardWrapper = styled.div`
+  width: 100%;
+  background: ${GRAY_11};
+  border-radius: 5px;
 `;
