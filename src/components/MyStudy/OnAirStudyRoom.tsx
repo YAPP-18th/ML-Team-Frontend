bac95bd271786686c9e8c0d27e31ef3005b84705
hoondeveloper <hoonskyn@naver.com>
2021-05-20 23:04:06 +0900

    fix: OnAirStudyRoom 문구 수정

--- src/components/MyStudy/OnAirStudyRoom.tsx
+++ src/components/MyStudy/OnAirStudyRoom.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { StdTypoSubtitle1 } from '@shared/styled/Typography';
 import { StyledEmptyCardWrapper } from '@shared/styled/Common';
 import 'twin.macro';
+import { Button } from 'antd';
 
 const OnAirStudyRoom: React.FC = () => {
   return (
     <>
       <StyledEmptyCardWrapper tw="text-center space-y-5 py-20">
         <StdTypoSubtitle1 tw="text-gray-6">
-          현재 온에어 상태인 공부방이 없어요.
+          지금 진행중인 공부방이 없어요. 공부방을 만들어 공부를 시작해보세요.
         </StdTypoSubtitle1>
+        <Button type="primary">공부방 만들기</Button>
       </StyledEmptyCardWrapper>
     </>
   );
