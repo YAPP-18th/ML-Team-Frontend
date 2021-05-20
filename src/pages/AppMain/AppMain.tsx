db6dccd29e8ac976ba42953ff4f88d98b7991bc7
hoondeveloper <hoonskyn@naver.com>
2021-05-21 00:58:27 +0900

    fix: app/mystudy 라우트 컴포넌트 MyStudyRoot로 수정

--- src/pages/AppMain/AppMain.tsx
+++ src/pages/AppMain/AppMain.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Redirect, Route, Switch, useRouteMatch } from 'react-router-dom';
-import { MyStudy } from '@pages/AppMain/MyStudy/MyStudy';
 import Study from '@pages/AppMain/Study/Study';
 import OnBoarding from '@pages/AppMain/OnBoarding/OnBoarding';
 import { NotFound } from '@pages/NotFound/NotFound';
+import MyStudyRoot from '@pages/AppMain/MyStudy/MyStudyRoot';
 
 export const AppMain: React.FC = () => {
   const { path } = useRouteMatch();
@@ -14,7 +14,7 @@ export const AppMain: React.FC = () => {
         <Route path={`${path}`} exact={true}>
           <Redirect to={`${path}/mystudy`} />
         </Route>
-        <Route path={`${path}/mystudy`} component={MyStudy} />
+        <Route path={`${path}/mystudy`} component={MyStudyRoot} />
         <Route path={`${path}/study`} component={Study} />
         <Route path={`${path}/onboarding`} component={OnBoarding} />
         <Route path={`${path}/*`} component={NotFound} />
