433a0fa08d2b7c7d3dc9f992a100fce696fd9b43
hoondeveloper <hoonskyn@naver.com>
2021-05-22 14:26:00 +0900

    fix: MyStudy 스타일 수정

--- src/pages/AppMain/MyStudy/MyStudy.tsx
+++ src/pages/AppMain/MyStudy/MyStudy.tsx
@@ -6,13 +6,22 @@ import 'twin.macro';
 import { MainLayout } from '@components/Layouts/main/MainLayout';
 import MyStudyRoom from '@components/MyStudy/MyStudyRoom';
 import OnAirStudyRoom from '@components/MyStudy/OnAirStudyRoom';
+import { Button } from 'antd';
+import { Link, useRouteMatch } from 'react-router-dom';
 
 export const MyStudy: React.FC = () => {
+  const { path } = useRouteMatch();
+
   return (
     <MainLayout>
       <StyledRestrictedArea>
         <StyledMyStudyCard>
-          <StdTypoH4>내 공부방</StdTypoH4>
+          <div tw="flex items-center justify-between">
+            <StdTypoH4>내 공부방</StdTypoH4>
+            <Link to={`${path}/create`}>
+              <Button size="small">공부방 만들기</Button>
+            </Link>
+          </div>
           <MyStudyRoom />
         </StyledMyStudyCard>
         <StyledMyStudyCard>
