fed9d6c822e7405e36569be53c0d7095a974827b
hoondeveloper <hoonskyn@naver.com>
2021-05-23 20:28:20 +0900

    feat: LabelRequiredCircle 구현

--- src/shared/styled/Common.tsx
+++ src/shared/styled/Common.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import { GRAY_11 } from '@shared/styles/colors';
+import { GRAY_11, PRIMARY_8 } from '@shared/styles/colors';
 
 export const StyledRestrictedArea = styled.section<{ maxWidth?: number }>`
   max-width: ${(props) => props.maxWidth || 1194}px;
@@ -19,4 +19,14 @@ export const StyledBoxWrapper = styled.section`
   background: ${GRAY_11};
   padding: 40px 46px;
   border-radius: 10px;
+`;
+
+export const LabelRequiredCircle = styled.span`
+  display: inline-block;
+  vertical-align: text-top;
+  margin-left: 4px;
+  width: 6px;
+  height: 6px;
+  background: ${PRIMARY_8};
+  border-radius: 50%;
 `;
