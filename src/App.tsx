40884984d718206eaad5132e25a4728a19961992
hoondeveloper <hoonskyn@naver.com>
2021-05-25 19:18:23 +0900

    feat: recoil 패키지 설치

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import 'twin.macro';
 import './App.css';
 import { Main } from '@pages/Main';
+import { RecoilRoot } from 'recoil';
 
 const App: React.FC = () => {
   return (
-    <>
+    <RecoilRoot>
       <Main />
-    </>
+    </RecoilRoot>
   );
 };
 
