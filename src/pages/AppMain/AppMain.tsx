804b40ea4f66894b7c2a9ae02b0101475248bdfc
hoondeveloper <hoonskyn@naver.com>
2021-05-30 06:23:25 +0900

    fix: 온보딩 페이지 접근 제어

--- src/pages/AppMain/AppMain.tsx
+++ src/pages/AppMain/AppMain.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { Redirect, Route, Switch, useRouteMatch } from 'react-router-dom';
 import { Study } from '@pages/AppMain/Study/Study';
 import OnBoarding from '@pages/AppMain/OnBoarding/OnBoarding';
@@ -8,30 +8,41 @@ import useUser from '../../hooks/useUser';
 import { useHistory } from 'react-router';
 import { Report } from '@pages/AppMain/Report/Report';
 import { NoReport } from '@pages/AppMain/Report/NoReport';
+import { message } from 'antd';
+import ConditionalRoute from '@components/Common/ConditionalRoute';
 
 export const AppMain: React.FC = () => {
   const { path } = useRouteMatch();
   const user = useUser();
   const history = useHistory();
-
-  useEffect(() => {
-    // 403 오류 발생 시 온보딩 화면으로 이동
-    if (user.error?.response?.status === 403) {
-      history.replace('/app/onboarding');
-    }
+  const userFetchStatus = useMemo(() => {
+    return user.error?.response?.status;
   }, [user]);
 
   return (
     <>
       <Switch>
-        <Route path={`${path}`} exact={true}>
+        <ConditionalRoute
+          path={`${path}`}
+          exact={true}
+          redirectPath={`${path}/onboarding`}
+          condition={userFetchStatus !== 403}
+        >
           <Redirect to={`${path}/mystudy`} />
-        </Route>
+        </ConditionalRoute>
+
         <Route path={`${path}/mystudy`} component={MyStudyRoot} />
+
+        <ConditionalRoute
+          path={`${path}/onboarding`}
+          redirectPath={`${path}`}
+          condition={userFetchStatus === 403}
+        >
+          <OnBoarding />
+        </ConditionalRoute>
         <Route path={`${path}/study`} component={Study} />
         <Route path={`${path}/noreport`} component={NoReport} />
         <Route path={`${path}/report`} component={Report} />
-        <Route path={`${path}/onboarding`} component={OnBoarding} />
         <Route path={`${path}/*`} component={NotFound} />
       </Switch>
     </>
