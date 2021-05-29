2f11659cb9077628606b5e3be0ba12f0519d09a0
hoondeveloper <hoonskyn@naver.com>
2021-05-30 06:23:13 +0900

    fix/feat: StudyCard 입장 시 비밀번호 입력, 내가 만든 방이면 제외하기 구현

--- src/hooks/apis/deleteStudyRoom.ts
+++ src/hooks/apis/deleteStudyRoom.ts
@@ -3,7 +3,7 @@ import { API_END_POINT } from '@shared/common';
 import useAccessToken from '../useAccessToken';
 
 export default async function deleteStudyRoom(
-  id: string,
+  id: number,
   accessToken: string | null,
 ) {
   const result = await axios.delete(`${API_END_POINT}/api/study-rooms/${id}`, {
