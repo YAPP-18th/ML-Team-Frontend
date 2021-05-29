27b9720cf99758e8d71ddeb9ed688513d70f7a4f
YeonjuSeo <tjduswn1219@gmail.com>
2021-05-30 04:10:22 +0900

    fix: 레포트생성 오류 핸들링

--- src/components/layouts/report/ReportLayout.tsx
+++ src/components/layouts/report/ReportLayout.tsx
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyledRestrictedArea } from '@shared/styled/Common';
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
 import 'twin.macro';
 import locale from 'antd/es/date-picker/locale/ko_KR';
 import { Content } from 'antd/es/layout/layout';
+import { ajax, AjaxError } from 'rxjs/ajax';
+import { useLocalStorage } from '@rehooks/local-storage';
+import { API_ENDPOINT } from '@shared/common';
+import { ErrMsgStyle } from '@components/OnBoarding/OnBoardingStepTwo';
+
 // typographys
 import { StdTypoH2 } from '@shared/styled/Typography';
 
 // components
-import { Button, DatePicker, Space } from 'antd';
+import { DatePicker, Space, message } from 'antd';
 import { MainLayout } from '@components/Layouts/main/MainLayout';
 import { Moment } from 'moment';
 import { SetStateAction } from 'react';
@@ -28,7 +33,30 @@ export const ReportLayout = ({
   setMonth,
   setDate,
 }: IDateProps) => {
+  const [accessToken] = useLocalStorage('accessToken');
   const [report, setReport] = useState(false);
+
+  useEffect(() => {
+    ajax({
+      url: `${API_ENDPOINT}/api/reports?date=${0}&user_id=${5}`,
+      method: 'GET',
+      headers: {
+        authorization: `${accessToken}`,
+      },
+    }).subscribe({
+      next: (res) => console.log(res),
+      error: (err: AjaxError) => {
+        setReport(false);
+        if (err.status === 404) {
+          console.log(err);
+          message.error({
+            content: '학습레포트 생성에 실패했습니다',
+            style: { ErrMsgStyle },
+          });
+        }
+      },
+    });
+  }, [accessToken]);
 
   function onChange(value: Moment | null, dateString: string) {
     console.log(dateString);
@@ -37,6 +65,7 @@ export const ReportLayout = ({
     setMonth(Number(dateArr[0] + dateArr[1] + dateArr[2] + dateArr[3]));
     setMonth(Number(dateArr[5] + dateArr[6]));
     setDate(Number(dateArr[8] + dateArr[9]));
+
     // API요청해서 결과가 404/detail => noreport 반환
     // else (200) => report 반환
   }
--- src/pages/AppMain/Report/Report.tsx
+++ src/pages/AppMain/Report/Report.tsx
@@ -5,9 +5,13 @@ import { Moment } from 'moment';
 import tw from 'twin.macro';
 import locale from 'antd/es/date-picker/locale/ko_KR';
 import { Content } from 'antd/es/layout/layout';
+import { ajax, AjaxError } from 'rxjs/ajax';
+import { useLocalStorage } from '@rehooks/local-storage';
+import { API_ENDPOINT } from '@shared/common';
+import { ErrMsgStyle } from '@components/OnBoarding/OnBoardingStepTwo';
 
 // components
-import { Button, DatePicker, Space } from 'antd';
+import { message, DatePicker, Space } from 'antd';
 import { MainLayout } from '@components/Layouts/main/MainLayout';
 import { StyledRestrictedArea } from '@shared/styled/Common';
 
@@ -22,11 +26,16 @@ export interface IStudyData {
 // export interface IArrStudyData extends Array<IStudyData> {}
 
 export const Report = () => {
+  const [accessToken] = useLocalStorage('accessToken');
   const [report, setReport] = useState<boolean>(true);
   const [year, setYear] = useState(new Date().getFullYear());
   const [month, setMonth] = useState(new Date().getMonth() + 1);
   const [date, setDate] = useState(new Date().getDate());
-  const [data, setData] = useState<Array<IStudyData>>([]);
+  const [data, setData] = useState<Array<IStudyData>>([
+    { value: 8, name: '스마트폰' },
+    { value: 3, name: '조는중' },
+    { value: 2, name: '자리비움' },
+  ]);
 
   function onChange(value: Moment | null, dateString: string) {
     console.log(dateString);
@@ -38,11 +47,29 @@ export const Report = () => {
     // API요청해서 결과가 404/detail => noreport 반환
     // else (200) => report 반환
     // API 결과에 따라 setReport, setData
-    setData([
-      { value: 8, name: '스마트폰' },
-      { value: 2, name: '조는중' },
-      { value: 2, name: '자리비움' },
-    ]);
+
+    ajax({
+      url: `${API_ENDPOINT}/api/reports?date=${dateString}&user_id=${5}`,
+      method: 'GET',
+      headers: {
+        authorization: `${accessToken}`,
+      },
+    }).subscribe({
+      next: (res) => {
+        console.log(res);
+        //data가 들어오면 setReport(true) ➡ 들어오는 데이터로 Data 설정하기
+        //detail이 들어오면 setReport(false)
+      },
+      error: (err: AjaxError) => {
+        if (err.status === 404) {
+          console.log(err);
+          message.error({
+            content: '학습레포트를 불러오는 데 실패했습니다',
+            style: { ErrMsgStyle },
+          });
+        }
+      },
+    });
   }
 
   return (
@@ -62,8 +89,8 @@ export const Report = () => {
           </Space>
         </div>
         <Content>
-          {/* {report == true ? <MyReport StudyData={data} /> : <NoReport />} */}
-          {report == true ? <MyReport /> : <NoReport />}
+          {report == true ? <MyReport StudyData={data} /> : <NoReport />}
+          {/* {report == true ? <MyReport /> : <NoReport />} */}
         </Content>
       </StyledRestrictedArea>
     </MainLayout>
