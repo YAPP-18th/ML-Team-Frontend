ec1db44664fee7870786adaea188078e59f5eece
YeonjuSeo <tjduswn1219@gmail.com>
2021-06-30 19:21:13 +0900

    feat: Landing페이지 작성

---
+++ src/components/templates/LandingLayout.tsx
@@ -0,0 +1,182 @@
+import React, { useEffect, useState } from 'react';
+import { Button, Layout, Menu } from 'antd';
+import { Content, Footer, Header } from 'antd/es/layout/layout';
+import Logo from '@assets/images/logo.svg';
+import 'twin.macro';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { GRAY_10, PRIMARY_8 } from '@shared/styles/colors';
+import tw from 'twin.macro';
+import { StyledRestrictedArea } from '@shared/styled/Common';
+import { Body2Style } from '@shared/styled/Typography';
+import { MenuItemProps } from 'antd/lib/menu/MenuItem';
+import { MenuClickEventHandler } from 'rc-menu/lib/interface';
+import { useHistory, useRouteMatch } from 'react-router';
+import { Link } from 'react-router-dom';
+import { deleteFromStorage } from '@rehooks/local-storage';
+import { StdTypoBody1 } from '@shared/styled/Typography';
+
+const FooterStyle = css`
+  width: 100%;
+  padding: 0 30px 20px 30px;
+`;
+
+const StyledFooterInner = styled.div`
+  max-width: 1194px;
+  margin: 0 auto;
+
+  ${tw`flex justify-between items-center text-gray-3 font-medium border-t border-gray-9 py-5`}
+  ${Body2Style}
+`;
+
+const MenuStyle = css`
+  margin-left: 45px;
+  background: transparent !important;
+  flex: 1;
+`;
+
+type MenuKey = 'mystudy' | 'report';
+type MenuItemMap = {
+  [key in MenuKey]: MenuItem;
+};
+type MenuItem = { linkTo: string; title: string; disabled?: boolean };
+
+const menuItemMap: MenuItemMap = {
+  mystudy: {
+    linkTo: '/app/mystudy',
+    title: '내 학습',
+  },
+  report: {
+    linkTo: '/app/report',
+    title: '학습레포트',
+  },
+};
+
+export const LandingLayout: React.FC = ({ children }) => {
+  const [selectedMenu, setSelectedMenu] = useState<MenuKey>();
+  const history = useHistory();
+  const match = useRouteMatch<{ current: string }>('/app/:current');
+
+  const onMenuClick: MenuClickEventHandler = ({ key }) => {
+    history.push(menuItemMap[key as MenuKey]?.linkTo);
+  };
+
+  const logout = () => {
+    deleteFromStorage('accessToken');
+  };
+
+  useEffect(() => {
+    const currentRoute: MenuKey = match?.params?.current as MenuKey;
+    setSelectedMenu(currentRoute);
+  }, [match]);
+
+  return (
+    <>
+      <Layout>
+        <Header
+          tw="p-0"
+          css={css`
+            padding: 0 30px;
+            background: ${GRAY_10};
+            box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.25);
+          `}
+        >
+          <StyledRestrictedArea tw="flex items-center">
+            <Link to="/" tw="inline-flex flex-shrink-0">
+              <img src={Logo} tw="self-center" alt="STUDEEP" />
+            </Link>
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              css={MenuStyle}
+              selectedKeys={selectedMenu ? [selectedMenu] : []}
+              onClick={onMenuClick}
+            >
+              {Object.entries(menuItemMap).map((i) => (
+                <MenuItemOverride key={i[0]} disabled={i[1]?.disabled}>
+                  {i[1].title}
+                </MenuItemOverride>
+              ))}
+            </Menu>
+            <Button
+              type="primary"
+              size="small"
+              tw="justify-self-end flex-shrink-0"
+              onClick={() => {
+                history.push('/auth');
+              }}
+            >
+              <StdTypoBody1>시작하기</StdTypoBody1>
+            </Button>
+          </StyledRestrictedArea>
+        </Header>
+        <Content>{children}</Content>
+        <Footer css={FooterStyle}>
+          <StyledFooterInner>
+            <div tw="flex justify-between items-center space-x-10">
+              <img
+                src={Logo}
+                css={css`
+                  height: 20px;
+                `}
+                alt="STUDEEP"
+              />
+              <ul tw="flex space-x-10">
+                <li>서비스 이용약관</li>
+                <li>개인정보 처리방침</li>
+              </ul>
+            </div>
+            <span
+              tw="text-gray-6"
+              css={css`
+                font-family: 'Lexend', sans-serif;
+              `}
+            >
+              © STUDEEP All Rights Reserved.
+            </span>
+          </StyledFooterInner>
+        </Footer>
+      </Layout>
+    </>
+  );
+};
+
+const MenuItemOverride: React.FC<MenuItemProps> = ({ children, ...props }) => {
+  const MenuItemStyle = css`
+    padding: 0 5px !important;
+    margin-right: 30px;
+
+    &.ant-menu-item-selected {
+      background: transparent !important;
+      font-weight: 600;
+
+      &::before {
+        background: ${PRIMARY_8};
+      }
+    }
+
+    &::before {
+      transition: all 0.1s;
+      background: transparent;
+      height: 4px;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      content: '';
+      right: 0;
+      width: 100%;
+    }
+
+    &:hover {
+      background: transparent !important;
+    }
+  `;
+
+  return (
+    <>
+      <Menu.Item {...props} css={MenuItemStyle}>
+        {children}
+      </Menu.Item>
+    </>
+  );
+};
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,215 @@
+import React, { useEffect, useState } from 'react';
+import { LandingLayout } from '@components/templates/LandingLayout';
+import 'twin.macro';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { Button } from 'antd';
+import { useHistory } from 'react-router';
+import { Main } from '@pages/Main';
+// typography
+import {
+  StdTypoH1,
+  StdTypoH2,
+  StdTypoH3,
+  StdTypoH5,
+  StdTypoBody1,
+} from '@shared/styled/Typography';
+
+// colors
+import { GRAY_1, PRIMARY_4, GRAY_6, GRAY_10 } from '@shared/styles/colors';
+
+//images
+import StudyroomImg from '@assets/images/studyroom-page.svg';
+import ReportImg from '@assets/images/report-page.svg';
+import StudyCardImg from '@assets/images/study-card.svg';
+import RestCardImg from '@assets/images/rest-card.svg';
+import AwaitCardImg from '@assets/images/await-card.svg';
+import SleepCardImg from '@assets/images/sleep-card.svg';
+import SmartphoneCardImg from '@assets/images/smartphone-card.svg';
+import LogoImg from '@assets/images/logo-circle.svg';
+import RectangleImg from '@assets/images/rectangle.svg';
+
+export const Landing: React.FC = () => {
+  const history = useHistory();
+  return (
+    <LandingLayout>
+      <div
+        tw="flex flex-col items-center justify-center"
+        css={css`
+          padding-top: 140px;
+          padding-bottom: 290px;
+        `}
+      >
+        <div
+          tw="flex flex-col items-center justify-center mb-10"
+          css={css`
+            background: linear-gradient(to right, #f8a9cf, #878edd);
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+          `}
+        >
+          <StdTypoH1>내 꿈을 향해 깊은 곳으로</StdTypoH1>
+          <StdTypoH1>STUDEEP</StdTypoH1>
+        </div>
+        <Button
+          type="primary"
+          size="large"
+          onClick={() => {
+            history.push('/auth');
+          }}
+        >
+          <StdTypoH5>스터딥 시작</StdTypoH5>
+        </Button>
+      </div>
+      <div tw="grid grid-flow-col grid-cols-2 auto-rows-max flex">
+        <div
+          tw="relative"
+          css={css`
+            margin-right: 65px;
+          `}
+        >
+          <img src={StudyroomImg} alt="공부방 예시" />
+          <div
+            tw="flex absolute"
+            css={css`
+              bottom: -7%;
+              // bottom: -80px;
+              left: 46px;
+            `}
+          >
+            <div
+              css={css`
+                backgroundcolor: 'pink';
+              `}
+            ></div>
+            <StyledStatusCardImg src={StudyCardImg} alt="공부중" />
+            <StyledStatusCardImg src={RestCardImg} alt="휴식중" />
+            <StyledStatusCardImg src={AwaitCardImg} alt="자리비움" />
+            <StyledStatusCardImg src={SleepCardImg} alt="조는중" />
+            <StyledStatusCardImg src={SmartphoneCardImg} alt="스마트폰" />
+          </div>
+        </div>
+        <div>
+          <StdTypoH5 tw="text-primary-4 mb-5">공부방</StdTypoH5>
+          <StdTypoH2 tw="mb-10">공부방 특징</StdTypoH2>
+          <StyledFeatureContainer tw="mb-5">
+            <div tw="flex">
+              <img
+                src={LogoImg}
+                css={css`
+                  width: 26px;
+                  height: 26px;
+                  margin-right: 10px;
+                `}
+              />
+              <StdTypoH5 tw="mb-4 text-gray-3">
+                5초면 만들 수 있는 몰입 환경{' '}
+              </StdTypoH5>
+            </div>
+
+            <StdTypoBody1 tw="text-gray-6">
+              화면에 두 손만 나오게 해주세요. 스터딥은 스마트폰을 사용하는지,
+              자리를 비웠는지, 졸고 있는지 실시간으로 상태를 확인해서 몰입을
+              도와드려요.{' '}
+            </StdTypoBody1>
+          </StyledFeatureContainer>
+          <StyledFeatureContainer>
+            <div tw="flex">
+              <img
+                src={RectangleImg}
+                css={css`
+                  width: 26px;
+                  height: 26px;
+                  margin-right: 10px;
+                `}
+              />
+              <StdTypoH5 tw="mb-4 text-gray-3">
+                실시간으로 함께, 부담없는 캠스터디
+              </StdTypoH5>
+            </div>
+
+            <StdTypoBody1 tw="text-gray-6">
+              서로의 얼굴이 보이지 않아도, 실시간으로 함께 공부할 수 있어요.
+            </StdTypoBody1>
+          </StyledFeatureContainer>
+        </div>
+      </div>
+      <div
+        tw="grid grid-flow-col grid-cols-2 auto-rows-max"
+        css={css`
+          margin-top: 233px;
+          margin-bottom: 164px;
+        `}
+      >
+        <div tw="justify-self-end">
+          <StdTypoH5 tw="text-primary-4 mb-5">학습 레포트</StdTypoH5>
+          <StdTypoH2 tw="mb-10">학습 레포트 특징</StdTypoH2>
+
+          <StyledFeatureContainer
+            css={css`
+              margin-right: 65px;
+            `}
+          >
+            <div tw="flex">
+              <img
+                src={RectangleImg}
+                css={css`
+                  width: 26px;
+                  height: 26px;
+                  margin-right: 10px;
+                `}
+              />
+              <StdTypoH5 tw="mb-4 text-gray-3">
+                한눈에 보는 나의 학습태도
+              </StdTypoH5>
+            </div>
+
+            <StdTypoBody1 tw="text-gray-6">
+              공부를 끝낸 후, 학습 레포트를 통해 순수 공부시간, 오늘의 집중도,
+              달성률은 기본, 집중 분산 요인을 파악해서 더 나은 공부 습관을
+              만들어나가요.
+            </StdTypoBody1>
+          </StyledFeatureContainer>
+        </div>
+        <div>
+          <img src={ReportImg} alt="공부방 예시" />
+        </div>
+      </div>
+      <div
+        tw="flex flex-col justify-center items-center"
+        css={css`
+          width: 100%;
+          height: 331px;
+          background: linear-gradient(to right, #f8a9cf, #878edd);
+        `}
+      >
+        <StdTypoH3 tw="text-gray-2 mb-4">시작해보세요</StdTypoH3>
+        <StdTypoH5 tw="text-gray-4 mb-8">
+          당신의 꿈을 이루기까지, 스터딥이 함께할게요
+        </StdTypoH5>
+        <Button
+          type="primary"
+          size="large"
+          onClick={() => {
+            history.push('/auth');
+          }}
+        >
+          <StdTypoH5>스터딥 시작</StdTypoH5>
+        </Button>
+      </div>
+    </LandingLayout>
+  );
+};
+
+const StyledStatusCardImg = styled.img`
+  // width: 180px;
+  // height: 204px;
+  width: 20%;
+  height: auto;
+`;
+const StyledFeatureContainer = styled.div`
+  background-color: ${GRAY_10};
+  padding: 25px;
+  border-radius: 10px;
+  width: 481px;
+`;
