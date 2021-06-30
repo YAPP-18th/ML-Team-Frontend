786162f31dbbad5b10ff9f4f17780171b81d5735
hoondeveloper <hoonskyn9@gmail.com>
2021-07-01 01:26:37 +0900

    feat: Study Page Component 고도화/리팩토링 및 소켓 연결
    - StudyLayout 내용 이전
    - 소켓 통신 관련 기본 로직 구현

--- src/pages/AppMain/Study/Study.tsx
+++ src/pages/AppMain/Study/Study.tsx
@@ -1,7 +1,20 @@
-import React, { useEffect, useMemo, useState } from 'react';
-import { StudyRoom } from '@pages/AppMain/Study/StudyRoom';
+import React, { useEffect, useState } from 'react';
 import { StudyReady } from '@pages/AppMain/Study/StudyReady';
-import { StudyFinish } from '@pages/AppMain/Study/StudyFinish';
+import { io } from 'socket.io-client';
+import useUser from '../../../hooks/useUser';
+import { Socket } from 'socket.io-client/build/socket';
+import { useHistory } from 'react-router';
+import { css } from '@emotion/react';
+import { Button, Layout, Modal } from 'antd';
+import {
+  StdTypoH4,
+  StdTypoH5,
+  StdTypoSubtitle1,
+} from '@shared/styled/Typography';
+import ExitImg from '@assets/images/exit.svg';
+import { GRAY_10, GRAY_12, GRAY_8 } from '@shared/styles/colors';
+import { Header } from 'antd/es/layout/layout';
+import 'twin.macro';
 
 export enum StudyStep {
   NOTHING = 'NOTHING',
@@ -10,76 +23,182 @@ export enum StudyStep {
   STUDY_FINISH = 'STUDY_FINISH',
 }
 
-interface IStudyProps {
-  isPublic: boolean;
-}
-
 export interface ICurrentStudy {
   studyId: number;
   title: string;
   description: string;
 }
 
-export interface ITotalStudyData {
-  sets: number;
-  hours: number;
-  minutes: number;
-  seconds: number;
+export const Study = () => {
+  const [step, setStep] = useState<StudyStep>(StudyStep.NOTHING);
+  const [socket, setSocket] = useState<Socket>();
+  const user = useUser();
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+  const handleOk = () => {
+    setIsModalVisible(false);
+    // if (page == 'ready') {
+    //   history.push(`/app/mystudy`);
+    // } else {
+    //   setStep(StudyStep.STUDY_FINISH);
+    // }
+  };
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  useEffect(() => {
+    if (user?.data) {
+      console.log('Create Socket.IO Instance');
+
+      const _socket = io(`${'localhost'}:8000/study`, {
+        reconnection: true,
+        forceNew: false,
+        auth: { user_id: user.data?.id },
+        transports: ['websocket'],
+        reconnectionAttempts: 3,
+      });
+      _socket.on('response', (res) => {
+        console.log(res);
+      });
+      _socket.on('disconnect', function () {
+        console.log('Client disconnected.');
+      });
+      setSocket(_socket);
+    }
+  }, [user]);
+
+  // useEffect(() => {
+  //   if (socket) {
+  //     console.log('Do Join Room');
+  //     socket.emit('joinRoom', 'f83e317a-9a0d-44c6-8e92-249f9a75fdb9');
+  //   }
+  // }, [socket]);
+
+  return (
+    <Layout
+      css={css`
+        height: 100%;
+      `}
+    >
+      <Header css={HeaderStyle}>
+        <div tw="flex">
+          <StdTypoH5>공부방 이름</StdTypoH5>
+          {/*{isPublic == false && <img src={PrivateImg} alt="비밀방" />}*/}
+        </div>
+
+        <Button
+          tw="bg-gray-10 border-none flex items-center hover:bg-gray-9"
+          shape="round"
+          type="primary"
+          onClick={showModal}
+        >
+          <img
+            css={css`
+              width: 24px;
+              height: 24px;
+              margin-right: 8px;
+              display: inline-block;
+            `}
+            src={ExitImg}
+            alt="공부종료"
+          />
+          <span>공부 종료하기</span>
+        </Button>
+
+        <Modal
+          visible={isModalVisible}
+          closable={false}
+          onOk={handleOk}
+          onCancel={handleCancel}
+          keyboard={false}
+          bodyStyle={{
+            height: '148px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          css={css`
+            .ant-modal-footer {
+              display: flex;
+              padding: 0;
+              border: none;
+            }
+          `}
+          footer={
+            <>
+              <StyledModalButton role="Cancel" func={handleCancel} />
+              <StyledModalButton role="Ok" func={handleOk} />
+            </>
+          }
+        >
+          <div>
+            <StdTypoH4 tw="text-gray-2">공부를 종료할까요?</StdTypoH4>
+          </div>
+        </Modal>
+      </Header>
+
+      <StudyReady setStep={setStep} />
+      {/*{socket?.connected ? (*/}
+      {/*  <>*/}
+      {/*  </>*/}
+      {/*) : (*/}
+      {/*  <div>소켓 접속 중</div>*/}
+      {/*)}*/}
+    </Layout>
+  );
+};
+
+const HeaderStyle = css`
+  height: 80px;
+  padding: 0 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: white;
+  background: ${GRAY_12};
+  border-bottom-width: 0.1px;
+  border-color: ${GRAY_8};
+`;
+
+interface IModalButtonProps {
+  role: string;
+  func: any;
 }
 
-export const Study = ({ isPublic }: IStudyProps) => {
-  const [currentStudy, setCurrentStudy] = useState<ICurrentStudy>();
-  const [step, setStep] = useState<StudyStep>(StudyStep.NOTHING);
-  const [totalData, setTotalData] = useState<number[]>([]);
-  const componentRendered = useMemo(() => {
-    switch (step) {
-      case StudyStep.STUDY_READY:
-        return (
-          <StudyReady
-            isPublic={isPublic}
-            setStep={setStep}
-            currentStudy={currentStudy}
-          />
-        );
-      case StudyStep.STUDY_ROOM:
-        return (
-          <StudyRoom
-            isPublic={isPublic}
-            setStep={setStep}
-            setTotalData={setTotalData}
-            currentStudy={currentStudy}
-          />
-        );
-      case StudyStep.STUDY_FINISH:
-        return (
-          <StudyFinish totalData={totalData} currentStudy={currentStudy} />
-        );
-    }
-  }, [step]);
+const StyledModalButton = ({ role, func }: IModalButtonProps) => {
+  return (
+    <Button
+      tw="rounded-none"
+      css={[
+        css`
+          flex: 1;
+          height: 74px;
+          margin: 0 !important;
+        `,
+        role == 'Cancel'
+          ? css`
+              border-bottom-left-radius: 10px;
+              background-color: ${GRAY_8};
 
-  // 페이지에 처음 들어왔을 때만 실행
-  useEffect(() => {
-    // 서버에서 이 사람이 어떤 step에 있는지 알아본다.
-    const currentStepFromServer = step;
-    switch (currentStepFromServer) {
-      case StudyStep.NOTHING:
-        // 공부방 입장 API
-        setStep(StudyStep.STUDY_READY);
-        break;
-      case StudyStep.STUDY_READY:
-        // 현재 접속 중인 공부방의 정보를 받아와서 setCurrentStudy
-        setStep(StudyStep.STUDY_READY);
-        break;
-      case StudyStep.STUDY_ROOM:
-        // 현재 접속 중인 공부방의 정보를 받아와서 setCurrentStudy
-        setStep(StudyStep.STUDY_ROOM);
-        break;
-      case StudyStep.STUDY_FINISH:
-        // 현재 접속 중인 공부방의 정보를 받아와서 setCurrentStudy
-        setStep(StudyStep.STUDY_FINISH);
-        break;
-    }
-  }, []);
-
-  return <>{componentRendered}</>;
+              &:hover {
+                background-color: ${GRAY_10};
+              }
+            `
+          : css`
+              border-bottom-right-radius: 10px;
+            `,
+      ]}
+      key={role == 'Cancel' ? 'keep' : 'quit'}
+      type={role == 'Cancel' ? 'default' : 'primary'}
+      onClick={func}
+    >
+      <StdTypoSubtitle1>
+        {role == 'Cancel' ? '조금 더 해볼래요' : '네, 그만할래요'}
+      </StdTypoSubtitle1>
+    </Button>
+  );
 };
