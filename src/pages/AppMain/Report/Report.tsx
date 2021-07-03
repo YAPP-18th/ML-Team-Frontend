d49e60744c9f7091f9db70b3ce6ee7928fdc2873
hoondeveloper <hoonskyn@cau.ac.kr>
2021-07-03 10:12:01 +0900

    fix: 레포트 로딩 구현, 데이터 fetch 제대로 안되는 현상 수정 (긴급)

--- src/pages/AppMain/Report/Report.tsx
+++ src/pages/AppMain/Report/Report.tsx
@@ -12,7 +12,7 @@ import { useLocalStorage } from '@rehooks/local-storage';
 import { Link, useRouteMatch } from 'react-router-dom';
 
 // components
-import { message, DatePicker, Space } from 'antd';
+import { message, DatePicker, Space, Spin } from 'antd';
 import { MainLayout } from '@components/templates/MainLayout';
 import { StyledRestrictedArea } from '@shared/styled/Common';
 
@@ -29,6 +29,7 @@ export interface IStudyData {
 export const Report = () => {
   const [date, setDate] = useState<Moment>();
   const [report, setReport] = useState<IReport[]>();
+  const [loading, setLoading] = useState(false);
   const user = useUser();
   const { path } = useRouteMatch();
   const [accessToken] = useLocalStorage('accessToken');
@@ -36,6 +37,7 @@ export const Report = () => {
   function onChange(value: Moment | null) {
     if (value) {
       setDate(value);
+      setLoading(true);
       getReport(
         {
           date: value?.format('yyyy-MM-DD'),
@@ -50,38 +52,46 @@ export const Report = () => {
           console.log(err);
           setReport([]);
           throw err;
+        })
+        .finally(() => {
+          setLoading(false);
         });
     }
   }
   useEffect(() => {
-    onChange(moment());
-  }, []);
+    if (user) {
+      onChange(moment());
+    }
+  }, [user]);
 
   return (
     <MainLayout>
       <StyledRestrictedArea>
-        <div tw="flex justify-between items-center mt-20 mb-7 ">
-          <StdTypoH2 tw="text-gray-1 ">
-            {date?.format('yyyy년 MM월 DD일') || '불러오는 중'}
-          </StdTypoH2>
-          <Space direction="vertical">
-            <DatePicker
-              locale={locale}
-              tw="mb-6"
-              style={{ width: '300px' }}
-              defaultValue={moment()}
-              onChange={onChange}
-            />
-          </Space>
-        </div>
-        <Content>
-          {/* <div>{JSON.stringify(report)}</div> */}
-          {JSON.stringify(report) !== '[]' ? (
-            <MyReport StudyData={report} />
-          ) : (
-            <NoReport />
-          )}
-        </Content>
+        <Spin spinning={loading} size="large">
+          <div tw="flex justify-between items-center mt-20 mb-7 ">
+            <StdTypoH2 tw="text-gray-1 ">
+              {date?.format('yyyy년 MM월 DD일') || '불러오는 중'}
+            </StdTypoH2>
+            <Space direction="vertical">
+              <DatePicker
+                locale={locale}
+                tw="mb-6"
+                style={{ width: '300px' }}
+                defaultValue={moment()}
+                disabled={loading}
+                onChange={onChange}
+              />
+            </Space>
+          </div>
+          <Content>
+            {/* <div>{JSON.stringify(report)}</div> */}
+            {JSON.stringify(report) !== '[]' ? (
+              <MyReport StudyData={report} />
+            ) : (
+              <NoReport />
+            )}
+          </Content>
+        </Spin>
       </StyledRestrictedArea>
     </MainLayout>
   );
